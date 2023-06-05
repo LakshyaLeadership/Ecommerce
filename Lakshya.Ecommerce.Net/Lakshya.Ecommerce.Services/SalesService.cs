e4fd6d86d0ef9046f6f8a31edb3fa9bfde28fca7
ab@lakshyaleaders.com <CleanOffice@2021>
2023-06-05 18:42:01 +0530

    data sources of fast report

--- Lakshya.Ecommerce.Net/Lakshya.Ecommerce.Services/SalesService.cs
+++ Lakshya.Ecommerce.Net/Lakshya.Ecommerce.Services/SalesService.cs
@@ -1,41 +1,41 @@
-﻿using Lakshya.Ecommerce.Services.Models;
+using Lakshya.Ecommerce.Services.Models;
 using Lakshya.Ecommerce.Repositories;
 using Lakshya.Ecommerce.Repositories.Entities;
 using AutoMapper;
+using FastReport;
+using FastReport.Export.Pdf;
 
 namespace Lakshya.Ecommerce.Services
 {
 
-    public class SalesService : ISalesService
+  public class SalesService : ISalesService
+  {
+    private readonly ISalesRepository _salesRepository;
+    private readonly IMapper mapper;
+
+    public SalesService(ISalesRepository salesRepository, IMapper mapper)
     {
-        private readonly ISalesRepository _salesRepository;
-        private readonly IMapper mapper;
-
-        public SalesService(ISalesRepository salesRepository, IMapper mapper)
-        {
-            _salesRepository = salesRepository;
-            this.mapper = mapper;
-        }
-
-        public async Task<List<SaleModel>> GetSalesData(DateTime startDate, DateTime endDate)
-        {
-            return mapper.Map<List<SaleModel>>(await _salesRepository.GetSalesData(startDate, endDate).ConfigureAwait(false));
-        }
-
-        public string GenerateReport(List<SaleModel> salesData)
-        {
-            //var report = new FastReport.Report();
-            //report.Load("path/to/sales_report_template.frx");
-
-            //report.RegisterData(salesData, "SalesData");
-            //report.Prepare();
-
-            //var exportOptions = new FastReport.Export.Pdf.PDFExport();
-            var reportFilePath = "path/to/generated_report.pdf";
-            //report.Export(exportOptions, reportFilePath);
-
-            return reportFilePath;
-        }
+      _salesRepository = salesRepository;
+      this.mapper = mapper;
     }
 
+    public async Task<List<SaleModel>> GetSalesData(DateTime startDate, DateTime endDate)
+    {
+      return mapper.Map<List<SaleModel>>(await _salesRepository.GetSalesData(startDate, endDate).ConfigureAwait(false));
+    }
+
+    public string GenerateReport(List<SaleModel> salesData)
+    {
+      var report = new Report();
+      report.Load("SalesReportTemplate.frx");
+
+      report.RegisterData(salesData, "SalesData");
+      report.Prepare();
+
+      var exportOptions = new PDFExport();
+      var reportFilePath = $"generated_report_{DateTime.UtcNow.Ticks}.pdf";
+      report.Export(exportOptions, reportFilePath);
+      return reportFilePath;
+    }
+  }
 }
