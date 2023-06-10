a7a7b2dd74f073dc0967fa4534943ad98ccbe2aa
ab@lakshyaleaders.com <CleanOffice@2021>
2023-06-10 08:37:45 +0530

    Material form generated

--- Lakshya.Ecommerce.AngularMaterial/src/app/app.module.ts
+++ Lakshya.Ecommerce.AngularMaterial/src/app/app.module.ts
@@ -4,15 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { CreateInventoryComponent } from './components/create-inventory/create-inventory.component';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateInventoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ Lakshya.Ecommerce.AngularMaterial/src/app/components/create-inventory/create-inventory.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-inventory',
+  templateUrl: './create-inventory.component.html',
+  styleUrls: ['./create-inventory.component.css']
+})
+export class CreateInventoryComponent implements OnInit {
+  createInventoryForm: FormGroup | undefined;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createInventoryForm = this.fb.group({
+      supplierName: ['', Validators.required],
+      name: ['', Validators.required],
+      sku: ['', Validators.required],
+      supplierEmail: ['', [Validators.required, Validators.email]],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      category: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!!this.createInventoryForm && this.createInventoryForm.valid) {
+      // Submit the form data to the API
+      const formData = this.createInventoryForm.value;
+      // Call the API with the form data
+      // ...
+    }
+  }
+}
